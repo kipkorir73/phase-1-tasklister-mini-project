d0df4ee0853db9e1b3c2194492da13a3eabd2f47
collins <kipkorirc583@gmail.com>
2023-06-25 11:56:05 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.getElementById('create-task-form');
+  const taskList = document.getElementById('tasks');
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault(); // Prevent form submission
+
+    const input = document.getElementById('new-task-description');
+    const taskDescription = input.value;
+
+    if (taskDescription) {
+      const taskItem = document.createElement('li');
+      taskItem.textContent = taskDescription;
+
+      const deleteButton = document.createElement('button');
+      deleteButton.textContent = 'Delete';
+      deleteButton.addEventListener('click', () => {
+        taskItem.remove();
+      });
+
+      taskItem.appendChild(deleteButton);
+      taskList.appendChild(taskItem);
+
+      input.value = ''; // Clear input field
+    }
+  });
 });
